1b2988fff424644b23b4e391685d587f8370d161
Gregory Lacerda <gregorylacerda@hotmail.com>
2022-03-07 09:42:01 -0300

    User delete

--- src/main/java/com/gregory/course/resources/UserResource.java
+++ src/main/java/com/gregory/course/resources/UserResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,5 +46,11 @@ public class UserResource {
 		//linha de codigo para criar uma url location que é necessaria para o created
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).body(obj); //created usado para que a resposta http deja 201, padrão para quando se insere uma informação
+	}
+	
+	@DeleteMapping(value = "/{id}")//anotation padrão para deletar objeto 
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();//noContent pois é um metodo que a responsta não tem corpo, e o codigo será 204
 	}
 }
